6c764f84c94d75e49a9e6352f2e98cc77197b8fd
alespouzar <ales.pouzar@gmail.com>
2016-10-18 19:34:50 +0200

    Remove INCLUDE_RECIPIENT_ID and MOVE_TO_FTP from ExportList

--- src/Keboola/SilverpopEx/Util/EngagePod.php
+++ src/Keboola/SilverpopEx/Util/EngagePod.php
@@ -203,9 +203,9 @@ class EngagePod {
                     "FILE_ENCODING" => "UTF-8",
                     "LIST_DATE_FORMAT" => "yyyy-MM-dd",
                     "INCLUDE_LEAD_SOURCE" => true,
-                    "INCLUDE_RECIPIENT_ID" => true,
+                    //"INCLUDE_RECIPIENT_ID" => true,
                     "EXPORT_FORMAT" => $format,
-                    "MOVE_TO_FTP" => 1,
+                    //"MOVE_TO_FTP" => 1,
                 ),
             ),
         );
